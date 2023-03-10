2c9e7f47d7f7e828e0f0f9bbf62fb2a2ed1374a7
JonathanHaws <jonathanmhaws1@gmail.com>
2023-03-10 16:53:59 -0700

    put / update routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -38,8 +38,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      { where: { id: req.params.id }
+      }
+    );
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with this id exists.' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag updated successfully.' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
